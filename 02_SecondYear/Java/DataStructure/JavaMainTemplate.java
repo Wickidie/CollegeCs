697b2df2af9317767f16e54c7009f6815a225161
Wickidie <72175210+MarvinAdinata@users.noreply.github.com>
2022-09-15 23:42:30 +0700

    Add files via upload

---
+++ 02_SecondYear/Java/DataStructure/JavaMainTemplate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+class JavaMainTemplate{
+  public static void main (String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    
+    
+    in.close();
+  } 
+}
