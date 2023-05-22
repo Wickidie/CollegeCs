106fcb791874680383e10fd8f619ffc3d99df12a
Wickidie <121784189+Wickidie@users.noreply.github.com>
2023-05-22 17:54:30 +0700

    add AES

---
+++ Java/AES/src/AESTest.java
@@ -0,0 +1,12 @@
+/**
+ * AESTest
+ */
+public class AESTest {
+        public static void main(String[] args) {
+                String hax = "32";
+                String hbx = "2b";
+                System.out.println(Integer.parseInt(hax, 16));
+                System.out.println(Integer.parseInt(hbx, 16));
+        }
+
+}
